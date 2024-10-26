9041ec35408e21d813001cdf71f4baeb6f635830
David L. Inman <davidlevere@gmail.com>
2024-10-26 12:51:20 -0400

    error output

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,32 @@ mod parser;
 
 use proc_macro::*;
 
+use parser::Error;
+
+pub (crate) enum PMAst {
+    Remove
+}
+
 #[proc_macro]
 pub fn pattern_matcher( input : TokenStream ) -> TokenStream {
-    todo!()
+    let input = input.into_iter().collect::<Vec<_>>();
+    let ast = parser::parse_pattern_matcher(&input, Span::call_site());
+    match ast {
+        Err(Error(span, message)) => { 
+            let mut error_code = format!("compile_error!(\"{}\")", message.join("\n"))
+                .parse::<TokenStream>()
+                .unwrap()
+                .into_iter()
+                .collect::<Vec<_>>();
+
+            for x in &mut error_code {
+                x.set_span(span);
+            }
+
+            error_code.into_iter().collect()
+        },
+        _ => todo!(),
+    }
 }
 
 
