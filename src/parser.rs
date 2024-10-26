0a4b45de85205bb53fd3d65c8d016e6dc75b4fd9
David L. Inman <davidlevere@gmail.com>
2024-10-26 12:51:39 -0400

    inc prog for parsing

--- src/parser.rs
+++ src/parser.rs
@@ -1,1 +1,24 @@
 
+use super::PMAst;
+
+use proc_macro::*;
+
+pub (crate) struct Error(pub (crate) Span, pub (crate) Vec<Box<str>>);
+pub (crate) type PResult<'a, T> = Result<(&'a [TokenTree], T, Span), Error>;
+
+
+pub (crate) fn parse_pattern_matcher(input : &[TokenTree], prev : Span) -> Result<PMAst, Error> {
+    let _ = parse_colon(input, prev)?;
+    Ok(PMAst::Remove)
+}
+
+fn parse_colon<'a>(input : &'a [TokenTree], prev : Span) -> PResult<'a, ()> {
+    match input {
+        [TokenTree::Punct(p), rest @ ..] if p.as_char() == ':' => Ok((rest, (), p.span())),
+        [x, ..] => Err(Error(x.span(), vec!["expected ':'".into()])),
+        [] => Err(Error(prev, vec!["unexpected end of stream".into()])),
+    }
+}
+
+
+//        [t @ TokenTree::Punct(p), rest @ ..] if p.as_char() == ';' => Ok((t, Input::new(rest, p.span()))),
